a9ee440bbcabf5f545cbbcd55507f6e5e00dea4c
MauLab <maulana.hasanudin.tif18@polban.ac.id>
2020-06-15 15:18:25 +0700

    membuat WorkRequest pertama

--- app/src/main/java/com/example/background/BlurViewModel.java
+++ app/src/main/java/com/example/background/BlurViewModel.java
@@ -18,16 +18,25 @@ package com.example.background;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
 import android.app.Application;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import com.example.background.workers.BlurWorker;
+
 public class BlurViewModel extends AndroidViewModel {
 
     private Uri mImageUri;
+    private WorkManager mWorkManager;
 
     public BlurViewModel(@NonNull Application application) {
+
         super(application);
+        mWorkManager = WorkManager.getInstance(application);
+
     }
 
     /**
@@ -35,7 +44,7 @@ public class BlurViewModel extends AndroidViewModel {
      * @param blurLevel The amount to blur the image
      */
     void applyBlur(int blurLevel) {
-
+        mWorkManager.enqueue(OneTimeWorkRequest.from(BlurWorker.class));
     }
 
     private Uri uriOrNull(String uriString) {
